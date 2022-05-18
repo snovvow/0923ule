d94765d4de87df58c295b77c752c457e07c0c46e
gaeunlee <62925109+0923ule@users.noreply.github.com>
2022-05-18 21:26:29 +0900

    [BOJ]암호만들기/ㅎ5

---
+++ 6주차_백트래킹/[1759]암호만들기.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int n, m;
+char c[20];
+char tmp[20];
+int pw[2] = { 0, }; // pw[0] = 모음의 개수, pw[1] = 자음의 개수
+
+
+void backtracking(int pre, int cnt);
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+
+	cin >> n >> m;
+	for (int i = 0;i < m;i++) {
+		cin >> c[i];
+	}
+	sort(c, c + m);
+	backtracking(-1, 0);
+	return 0;
+}
+
+void backtracking(int pre, int cnt) {
+	if (cnt == n) {
+		if (pw[0] >= 1 && pw[1] >= 2) { // 조건 만족
+			for (int i = 0;i < n;i++) {
+				cout << tmp[i];
+			}
+			cout << "\n";
+		}
+		return;
+	}
+	if (pre == m - 1) { // 암호 미완성
+		return;
+	}
+
+	for (int i = pre + 1;i < m;i++) {
+		tmp[cnt] = c[i];
+		if (tmp[cnt] == 'a' || tmp[cnt] == 'e' || tmp[cnt] == 'i' || tmp[cnt] == 'o' || tmp[cnt] == 'u') {
+			pw[0]++;
+			backtracking(i, cnt + 1);
+			pw[0]--;
+		}
+		else {
+			pw[1]++;
+			backtracking(i, cnt + 1);
+			pw[1]--;
+		}
+	}
+}
