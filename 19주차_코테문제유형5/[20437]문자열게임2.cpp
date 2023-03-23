ddba4ab6ea844320dd3319572d75182756e58f65
gaeunlee <62925109+0923ule@users.noreply.github.com>
2023-03-23 23:09:59 +0900

    [BOJ]문자열게임2.cpp

---
+++ 19주차_코테문제유형5/[20437]문자열게임2.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<vector>
+#include<cstring>
+#include<algorithm>
+using namespace std;
+
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int t;
+	cin >> t;
+	while (t--) {
+		string w;
+		int k;
+		int alpha[30] = { 0, };
+
+		int ans3 = 987654321;
+		int ans4 = -1;
+
+		cin >> w >> k;
+
+
+		for (int i = 0; i < w.size(); i++) {
+			alpha[w[i] - 'a']++;
+		}
+
+		for (int i = 0; i < w.size(); i++) {
+			if (alpha[w[i] - 'a'] < k) continue;
+
+			int cnt = 0;
+			for (int j = i; j < w.size(); j++) {
+				if (w[i] == w[j]) cnt++;
+				
+				if (cnt == k) {
+					ans3 = min(ans3, j - i + 1);
+					ans4 = max(ans4, j - i + 1);
+					break;
+				}
+			}
+		}
+		if (ans3 == 987654321 || ans4 == -1) cout << "-1\n";
+		else cout << ans3 << " " << ans4 << "\n";
+	}
+
+	return 0;
+}
